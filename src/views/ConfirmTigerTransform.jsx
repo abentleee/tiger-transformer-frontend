706eff392cbd1fc3a7311a5982976c63edd4f97c
abentleee <alanbentleypgh@gmail.com>
2023-05-04 11:25:13 -0400

    pass in wallet address to confirm view from list all view, implement confirm view

--- src/views/ConfirmTigerTransform.jsx
+++ src/views/ConfirmTigerTransform.jsx
@@ -1,7 +1,10 @@
-import { useLocation, Navigate } from 'react-router-dom';
+import { useLocation, Navigate, useNavigate } from 'react-router-dom';
+import Button from '../components/Button';
+import { shortenContractAddress } from '../utils/StringUtil';
 
 const ConfirmTigerTransformation = () => { 
     const location = useLocation();
+    const navigate = useNavigate();
 
     const styles = { 
         container: { 
@@ -10,26 +13,74 @@ const ConfirmTigerTransformation = () => {
             flexDirection: 'column',
             justifyContent: 'center',
             alignItems: 'center',
-            marginTop: '5%',
-            marginBottom: '5%',
-            marginLeft: '15%',
-            marginRight: '15%',
+        },
+        tigerContainer: { 
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+        buttonRowContainer: {
+            display: 'flex', 
+            flexDirection: 'row',
         },
         headerText: { 
             textAlign: 'center',
             fontWeight: 'bold',
             paddingBottom: '2%',
+            fontSize: '75%',
             textShadow: '2px 2px black',
-        }
+        },
+        bodyText: { 
+            textAlign: 'center',
+            fontWeight: 'bold',
+            fontSize: '75%',
+            textShadow: '2px 2px black',
+        },
+        selectedTigerImage: { 
+            width: 150,
+            height: 150,
+            margin: '2%',
+            border: '2px red solid',
+        },
     }
 
     if (location.state) {
+        const selectedTiger = location.state.selectedTiger;
+        const selectedAccount = location.state.selectedAccount;
+
         return (
             <div style={styles.container}>
                 <div style={styles.headerText}>
-                    Confirm Tiger Transform View
+                    Wallet: {shortenContractAddress(selectedAccount)}
+                    <br /><br/>
+                    Are you sure you want to transform this tiger? 
+                    <br />
+                    A small amount of gas will be charged to transform.
                 </div>
-                {location.state.selectedTiger.id} | {location.state.selectedTiger.imageUrl}
+                <div style={styles.tigerContainer}>
+                    <img 
+                        style={styles.selectedTigerImage}
+                        src={selectedTiger.imageUrl}
+                        key={selectedTiger.id}
+                        alt={selectedTiger.id}
+                    />
+                    <div style={styles.bodyText}>
+                        {selectedTiger.id}
+                    </div>
+                </div>
+                <div style={styles.buttonRowContainer}>
+                    <Button 
+                        text={'TRANSFORM'}
+                        onClick={() => console.log(`transform invoked!`)}
+                    />
+                    <Button 
+                        text={'HOME'}
+                        onClick={() => {
+                            navigate('/');
+                        }}
+                    />
+                </div>
             </div>
         );
     }
